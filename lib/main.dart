89950653f215fa58914cca99f6e6f4f4d7ba2c5b
juhyoungshin <juhyoung200410@gmail.com>
2022-09-12 22:16:38 +0900

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,107 @@
-import 'package:flutter/material.dart';
+//? CodeWithFlexz on Instagram
 
-void main() {
-  runApp(const MyApp());
+//* AmirBayat0 on Github
+//! Programming with Flexz on Youtube
+
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+///
+import '../data/hive_data_store.dart';
+import '../models/task.dart';
+import '../view/home/home_view.dart';
+
+Future<void> main() async {
+  /// Initial Hive DB
+  await Hive.initFlutter();
+
+  /// Register Hive Adapter
+  Hive.registerAdapter<Task>(TaskAdapter());
+
+  /// Open box
+  var box = await Hive.openBox<Task>("tasksBox");
+
+  /// Delete data from previous day
+  // ignore: avoid_function_literals_in_foreach_calls
+  box.values.forEach((task) {
+    if (task.createdAtTime.day != DateTime.now().day) {
+      task.delete();
+    } else {}
+  });
+
+  runApp(BaseWidget(child: const MyApp()));
+}
+
+class BaseWidget extends InheritedWidget {
+  BaseWidget({Key? key, required this.child}) : super(key: key, child: child);
+  final HiveDataStore dataStore = HiveDataStore();
+  @override
+  final Widget child;
+
+  static BaseWidget of(BuildContext context) {
+    final base = context.dependOnInheritedWidgetOfExactType<BaseWidget>();
+    if (base != null) {
+      return base;
+    } else {
+      throw StateError('Could not find ancestor widget of type BaseWidget');
+    }
+  }
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    return false;
+  }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Hive Todo App',
       theme: ThemeData(
-
-        primarySwatch: Colors.green,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            color: Colors.black,
+            fontSize: 45,
+            fontWeight: FontWeight.bold,
+          ),
+          subtitle1: TextStyle(
+            color: Colors.grey,
+            fontSize: 16,
+            fontWeight: FontWeight.w300,
+          ),
+          headline2: TextStyle(
+            color: Colors.white,
+            fontSize: 21,
+          ),
+          headline3: TextStyle(
+            color: Color.fromARGB(255, 234, 234, 234),
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+          ),
+          headline4: TextStyle(
+            color: Colors.grey,
+            fontSize: 17,
+          ),
+          headline5: TextStyle(
+            color: Colors.grey,
+            fontSize: 16,
+          ),
+          subtitle2: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w500,
+          ),
+          headline6: TextStyle(
+            fontSize: 40,
+            color: Colors.black,
+            fontWeight: FontWeight.w300,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: const HomeView(),
     );
   }
 }
